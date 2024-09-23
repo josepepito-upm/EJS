8f969825d2f191f80162088975809a8f989501d1
josepepito-upm <r.nuno@alumnos.upm.es>
2024-09-23 20:36:10 +0200

    * Finished the problem:     - declared variable 'i' for counting the number until it reaches the objective     - Removed boolean 'isPrime' (prints the result directly at stdout)
    //ganxo

--- Prime.java
+++ Prime.java
@@ -5,16 +5,14 @@ public class Prime {
     public static void main (String[] args) {
     
     Random random = new Random();
-    final int NUM = 2;
-    boolean isPrime = false;
+    final int NUM = random.nextInt(0, 100);
+        int i = 0;
 
     if(NUM >= 2) {
-        int i = 2;
+        i = 2;
     }
     while (NUM%i != 0) { i++; }
 
-    isPrime = NUM == i;
-
-    System.out.printf("%d %s primo. \n", NUM, isPrime ? "es" : "no es");  
+    System.out.printf("%d %s primo. \n", NUM, NUM == i ? "es" : "no es");  
     }
 }
